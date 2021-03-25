073cd6c500a86d2abfabee78870e5f3e6bbeb972
Silvio <silvio.dias@etec.sp.gov.br>
2021-03-24 22:30:35 -0300

    alterando a const getServerSideProps para getStaticProps e criando revalidate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head  from 'next/head'
-import {GetServerSideProps} from 'next'
+import {GetStaticProps} from 'next'
 
 import { SubscribeButton } from '../components/SubscribeButton'
 
@@ -37,7 +37,7 @@ export default function Home({product}: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IYfXnGV77g8dGHd8B6QLHXE')
 
 const product = {
@@ -51,6 +51,7 @@ const product = {
   return {
     props:{
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 hours
   }
 }
