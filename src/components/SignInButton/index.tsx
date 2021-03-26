a1d5437bc8b1a5bc8ea0550b9fb78df8bdcb9657
Silvio <silvio.dias@etec.sp.gov.br>
2021-03-26 20:50:51 -0300

    incluir o useSession do next-auth/client e dados coletas ao usuário logar na aplicacao

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,26 +1,31 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggedIn = true
-  return isUserLoggedIn ? (
+  const [session] = useSession()
+
+  console.log(session)
+
+  return session ? (
     <button
       type="button"
       className={styles.signButton}
+      onClick={() => signOut()}
     >
       <FaGithub
         color="#04d361"
       />
-      Silvio Dias
-
-      <FiX color="#737380" className={styles.closeIcon}/>
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={styles.signButton}
+      onClick={() => signIn('Github')}
     >
       <FaGithub
         color="#eba417"
