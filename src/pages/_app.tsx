6485f32c9efa8ada39ba481582061a5a643e4601
Silvio <silvio.dias@etec.sp.gov.br>
2021-03-26 20:52:19 -0300

    importando o Provider de next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import { AppProps } from 'next/App'
 import { Header } from '../components/Header'
+import {Provider as NexAtuthProvider} from 'next-auth/client'
 
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NexAtuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NexAtuthProvider>
   )
 }
 
